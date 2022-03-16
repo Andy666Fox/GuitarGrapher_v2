9e32c296cbd3250d911e689a85cbb8550e4a65ad
GoDoVoReZ <arsale3005@gmail.com>
2022-03-16 18:46:31 +0300

    minor changes

--- w_model.py
+++ w_model.py
@@ -5,19 +5,21 @@ import numpy as np
 from torch import classes
 
 
-class Model(ABC):
+class AModel(ABC):
 
     def __init__(self):
         self.model = keras.models.load_model('./model')
         self.classes = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'Barrel': 8, 'Flage': 9, 'PoffHon': 10, 'Slap': 11, 'Slide': 12}
 
-    def feature_exctractor(self, auddata):
-        data = lb.feature.mfcc(auddata, n_mfcc=128)
-        data= np.mean(data, axis=1)
 
-        return data
+    def _predict(self, aud) -> str:
 
-    def predict(self, aud) -> str:
+        def feature_exctractor(auddata):
+            data = lb.feature.mfcc(auddata, n_mfcc=128)
+            data= np.mean(data, axis=1)
+
+            return data
+
         to_predict = feature_exctractor(aud)
         classid = np.argmax(self.model.predict(to_predict)[0])
 
