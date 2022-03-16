b50fbc0ab3245bd49f99bf8d19ec67bed1e674e5
GoDoVoReZ <arsale3005@gmail.com>
2022-03-16 19:11:05 +0300

    minor changes

--- w_model.py
+++ w_model.py
@@ -12,11 +12,11 @@ class AModel(ABC):
         self.classes = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'Barrel': 8, 'Flage': 9, 'PoffHon': 10, 'Slap': 11, 'Slide': 12}
 
 
-    def _predict(self, aud) -> str:
+    def dpredict(self, aud) -> str:
 
         def feature_exctractor(auddata):
             data = lb.feature.mfcc(auddata, n_mfcc=128)
-            data= np.mean(data, axis=1)
+            data = np.mean(data, axis=1)
 
             return data
 
